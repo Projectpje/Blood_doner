9c84d246c8721ee050672d9edeeee5f3929f3c50
projectpje221 <projectpje221@gmail.com>
2021-01-22 01:39:42 +0530

    added navigation

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
+import * as GoogleSignIn from 'expo-google-sign-in';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const firebaseConfig = {
   databaseURL: 'https://blooddoner-19be1-default-rtdb.firebaseio.com/',
@@ -19,6 +22,16 @@ if (!firebase.apps.length) {
   firebase.app(); // if already initialized, use that one
 }
 
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -27,14 +40,30 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-   
+
+    console.log("component did mount");
+  //  this.signInAsync();
   }
+
+  signInAsync = async () => {
+    try {
+      await GoogleSignIn.askForPlayServicesAsync();
+      const { type, user } = await GoogleSignIn.signInAsync();
+      if (type === 'success') {
+        this._syncUserWithStateAsync();
+      }
+    } catch ({ message }) {
+      alert('login: Error:' + message);
+    }
+  };
 
   render() {
     return (
-      <View>
-        <Text> App </Text>
-      </View>
+      <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
     );
   }
 }
