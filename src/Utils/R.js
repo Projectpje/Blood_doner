028bb275338526a9a91b15f6562aa419e287c556
projectpje221 <projectpje221@gmail.com>
2021-01-30 14:50:37 +0530

    Create R.js

---
+++ src/Utils/R.js
@@ -0,0 +1,17 @@
+import * as Images from './Images';
+import * as Dimension from './Constants/Dimensions';
+import * as HelperFunctions from './HelperFunctions';
+import * as Colors from './Constants/Color';
+import * as Constants from './Constants/Constants';
+import * as ChipsData from './Constants/ChipsData';
+
+const R = {
+    Images,
+    Dimension,
+    HelperFunctions,
+    Colors,
+    Constants,
+    ChipsData
+}
+
+export default R;
